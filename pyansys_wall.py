393ddc31352389ff81a1ab59e5262384e7792f43
mirtft <mirtft@gmail.com>
2025-03-06 13:22:15 -0500

    got the driver function to work

--- pyansys_wall.py
+++ pyansys_wall.py
@@ -1,2 +1,6 @@
-from explode_assembly import *
-explode('narrow_am_inex_wall_v2.assembly.json')
+
+def run_explode():
+    from explode_assembly import explode
+    explode('narrow_am_inex_wall_v2.assembly.json')
+
+
