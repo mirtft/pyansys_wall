3f97bcdf311e670ea9e0289da8af7397749e37bb
mirtft <mirtft@gmail.com>
2025-03-07 08:31:26 -0500

    added functions to driver script

--- pyansys_wall.py
+++ pyansys_wall.py
@@ -3,4 +3,15 @@ def run_explode():
     from explode_assembly import explode
     explode('narrow_am_inex_wall_v2.assembly.json')
 
+def launch_pyansys():
+    # this function will start PyANSYS
+    from ansys.mapdl.core import launch_mapdl
 
+    mapdl = launch_mapdl()
+
+#def import_electrical():
+    # this will import the eletrical chaseway geometry as a beam in ANSYS
+    
+
+# run_explode()
+launch_pyansys
