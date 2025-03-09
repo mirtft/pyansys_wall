d0de2e6c88ebe4dd5005196f6ad818d70fec2f02
Scott Tomlinson <mirtft@users.noreply.github.com>
2025-03-09 18:59:05 -0400

    working on launching MAPDL and Mechanical

--- pyansys_wall.py
+++ pyansys_wall.py
@@ -1,12 +1,28 @@
 # Description: This script will import the geometry of the wall and the electrical chase way into ANSYS
 
+# # import parallels desktop api so we can call Parllels Windows 10 VM
+# import prlsdkapi
+# def start_parallels_vm(vm_name):
+#     prlsdkapi.Initialize()
+#     vm = prlsdkapi.VirtualMachine.Open(vm_name)
+#     vm.Start()
+# start_parallels_vm("Win10")  # Replace "VM_Name" with the actual name of your VM
+
+
 # this function will start PyANSYS
 from ansys.tools.path import find_mechanical
 from ansys.mechanical.core import launch_mechanical
 from ansys.mapdl.core import launch_mapdl
 from ansys.tools.path import save_mechanical_path
-save_mechanical_path('192.168.18.33)
-#save_mechanical_path('/Users/s/Applications\ \(Parallels\)/\{1fe107cd-77f8-4a53-ad5d-e40531ed3584\}\ Applications.localized/') #Workbench\ 2025\ R1.app/')
-find_mechanical()
-mechanical = launch_mechanical()
-mapdl = launch_mapdl()
+
+import os
+wbpath = os.path.join("/Volumes", "[C] Win10", "Program Files", "ANSYS Inc", "ANSYS Student", "v251", "aisol", "bin", "winx64")
+wbexec_file = os.path.join(wbpath, "AnsysWBU.exe")
+
+MAPDLpath = os.path.join("/Volumes", "[C] Win10", "Program Files", "ANSYS Inc", "ANSYS Student", "v251", "ansys", "bin", "winx64")
+MAPDLexec_file = os.path.join(MAPDLpath, "MAPDL.exe")
+
+#save_mechanical_path(wbpath)
+
+mechanical = launch_mechanical(wbexec_file)
+mapdl = launch_mapdl(MAPDLexec_file)
